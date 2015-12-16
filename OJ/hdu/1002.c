377e27b9eae1e9749ac1dc283891756949be3f9c
NEWPLAN <enwplan001@163.com>
2015-12-16 22:20:51 +0800

    modified files

--- OJ/hdu/1002.c
+++ OJ/hdu/1002.c
@@ -1,26 +1,37 @@
+/*
+Problem : 1002 ( A + B Problem II )     Judge Status : Accepted
+RunId : 15878392    Language : C    Author : NEWPLAN
+Code Render Status : Rendered By HDOJ C Code Render Version 0.01 Beta
+*/
 #include <stdio.h>
 #include <string.h>
-#define MAXLEN 1000
+#define MAXLEN 1005
 char a1[MAXLEN];
 char a2[MAXLEN];
 static int v1[MAXLEN];
 static int v2[MAXLEN];
 static int v3[MAXLEN];
 int i,j,n,L,z;
-void main(void)
+int main(void)
 {
     scanf("%d",&n);
     for (j=0; j<n; j++)
     {
+        memset(a1,0,MAXLEN);
+        memset(a2,0,MAXLEN);
+
         scanf("%s%s",a1,a2);
 
         L=strlen(a1);
-        for (i=0; i<L; i++) v1[i]=a1[L-1-i]-'0';
+        for (i=0; i<L; i++)
+            v1[i]=a1[L-1-i]-'0';
 
         L=strlen(a2);
-        for (i=0; i<L; i++) v2[i]=a2[L-1-i]-'0';
+        for (i=0; i<L; i++)
+            v2[i]=a2[L-1-i]-'0';
 
-        for (i=0; i<MAXLEN; i++) v3[i]=v1[i]+v2[i];
+        for (i=0; i<MAXLEN; i++)
+            v3[i]=v1[i]+v2[i];
 
         for (i=0; i<MAXLEN; i++)
         {
@@ -49,9 +60,15 @@ void main(void)
             {
                 printf("%d",v3[i]);
             }
+            v3[i]=0;
+            v2[i]=0;
+            v1[i]=0;
         }
         if (z==0) printf("0");
-
-        printf("\n");
+        if(j<n-1)
+            printf("\n\n");
+        else
+            printf("\n");
     }
+    return 0;
 }
