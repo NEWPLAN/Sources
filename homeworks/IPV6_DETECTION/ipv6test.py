347410624a1c91b242ec382d26991845dbdcce42
NEWPLAN <enwplan001@163.com>
2017-03-30 11:58:57 +0800

    update & add new function

--- homeworks/IPV6_DETECTION/ipv6test.py
+++ homeworks/IPV6_DETECTION/ipv6test.py
@@ -4,31 +4,31 @@
 import dns.resolver, time;
 import threading;
 import urllib.request
-import datetime;
-#from datetime import *
-
+# import datetime;
+from datetime import datetime
 
 domains = []
+
 
 def my_query(addr, iptype):
     try:
         answer = dns.resolver.query(addr, iptype);
-        #print(answer[0])
+        # print(answer[0])
         return answer[0];
     except dns.resolver.NoAnswer:
-        #print("No " + iptype)
+        # print("No " + iptype)
         return str('NO' + iptype);
     except dns.resolver.NXDOMAIN:
-        #print("No such domain")
+        # print("No such domain")
         return 'NODOMAIN';
     except dns.resolver.NoNameservers:
-        #print('No answer');
+        # print('No answer');
         return 'NOANSWER';
     except dns.resolver.YXDOMAIN:
-        #print('domain is too long');
+        # print('domain is too long');
         return 'TOOLONG';
     except dns.exception.Timeout:
-        #print('time out');
+        # print('time out');
         return 'TIMEOUT';
     except KeyboardInterrupt:
         print("\nGoodbye!")
@@ -36,17 +36,19 @@ def my_query(addr, iptype):
         print('unknown exception ', e, addr, iptype)
         return None;
 
-#domains=['abs','eee','rrr']
+
+# domains=['abs','eee','rrr']
 
 def save_result(filename=None):
     if filename is None:
-        filename=str(datetime.datetime.now()).replace(' ','_').replace(':','_')+'.csv'
-    counts=1
-    with open(filename,'w') as f:
+        filename = str(datetime.now()).replace(' ', '_').replace(':', '_') + '.csv'
+    counts = 1
+    with open(filename, 'w') as f:
         for index in domains:
-            f.write(str(counts)+','+str(index)+'\n')
-            counts+=1
+            f.write(str(counts) + ',' + str(index) + '\n')
+            counts += 1
     pass;
+
 
 def load_domain(filename=None):
     if filename is None:
@@ -57,7 +59,7 @@ def load_domain(filename=None):
         with open(filename, 'r') as f:
             for line in f:
                 index, addr = line.strip('\r\n').split(',')
-                #print('%s\t%s' % (index, r'www.' + addr))
+                # print('%s\t%s' % (index, r'www.' + addr))
                 domains.append(addr)
         print(len(domains))
         return
@@ -65,39 +67,96 @@ def load_domain(filename=None):
         pass;
     pass;
 
-def query_domain(start,end):
-    #print(start,end)
+
+def query_domain(start, end):
+    # print(start,end)
     global domains
-    for index in range(start,end):
-        rtev4 = my_query(domains[index], 'A');
-        rtev6 = my_query(domains[index], 'AAAA');
+    for index in range(start, end):
+        rtev4 = my_query(domains[index], 'A')#query IPV4
+        rtev6 = my_query(domains[index], 'AAAA')#query IPV6
         if rtev4 is None:
-            domains[index] = domains[index] +', '
+            domains[index] = domains[index] + ', '
         else:
-            domains[index] = domains[index] + ','+str(rtev4);
+            domains[index] = domains[index] + ',' + str(rtev4)
         if rtev6 is None:
-            domains[index] = domains[index] +', '
+            domains[index] = domains[index] + ', '
         else:
-            domains[index] = domains[index] + ','+str(rtev6);
-        #print(index,domains[index])
-        time.sleep(0.001);
+            domains[index] = domains[index] + ',' + str(rtev6)
+            # print(index,domains[index])
+            # time.sleep(0.001);
+
     return
 
 
+# 2402:f000:1:404:166:111:4:100
+# 166.111.4.100
+
+def request_url(ipv4=None, ipv6=None, loops=3):
+    if ipv4 is None or ipv6 is None:
+        return None
+        pass
+    interval = 0
+    html4 = None
+    html6 = None
+    for i in range(loops):
+        try:
+            startTime = datetime.now().microsecond
+            retval = urllib.request.urlopen('http://' + ipv4, timeout=10)
+            html4 = retval.read()
+            print('from v4', html4)
+            endTime = datetime.now().microsecond
+            interval = endTime - startTime
+        except Exception as e:
+            print(e, 'error in request url : ', ipv4)
+            pass;
+    for i in range(loops):
+        try:
+            startTime = datetime.now().microsecond
+            retval = urllib.request.urlopen('http://[' + ipv6 + ']', timeout=10)
+            # print(retval.info())
+            # print(retval.geturl())
+            html6 = retval.read()
+            print('from v6', html6)
+            endTime = datetime.now().microsecond
+            interval = endTime - startTime
+        except Exception as e:
+            print(e, 'error in request url : ', ipv6)
+            pass;
+    if html4 is None or html6 is None:
+        return None
+    # elif html6 is None:
+    #     return None;
+    else:
+        if html4 == html6:
+            print('no different from v4 VS v6')
+        else:
+            print('some different in v4 VS v6')
+        print(html4)
+        print(html6)
+
+    pass;
+
+
 def main():
+
+    time1=datetime.now()
     load_domain('top-1m.csv')
+    time2=datetime.now()
 
-    thread_id=[]
-    N=len(domains)//1000
-    print('loading %d domain from top-1m.csv done , now testing dns using %d threads!' % (len(domains), N))
+    thread_id = []
+    N = len(domains) // 1000
+    print('loading %d domain from top-1m.csv using %f Seconds;\
+    \n now testing dns using %d threads!' % (len(domains),(time2-time1).total_seconds(), N))
 
-    for index in range(0,len(domains)//1000):
-        thread_id.append(threading.Thread(target=query_domain,args=(index*1000,(index+1)*1000)))
     for index in range(0, len(domains) // 1000):
-        thread_id[index].start();
+        thread_id.append(threading.Thread(target=query_domain,
+                                          args=(index * 1000, (index + 1) * 1000)))
     for index in range(0, len(domains) // 1000):
-        thread_id[index].join();
-    print('all finished')
+        thread_id[index].start()
+    for index in range(0, len(domains) // 1000):
+        thread_id[index].join()
+    time1=datetime.now()
+    print('all finished using %d Seconds'%(time1-time2).total_seconds())
     save_result()
     pass;
 
@@ -132,6 +191,7 @@ def Process(url, n):
     print('\n')
 
 
+
 '''
 import urllib.request
 
@@ -148,10 +208,12 @@ if __name__ == '__main__':
     #print(html)
 
 '''
+# 2402:f000:1:404:166:111:4:100
+# 166.111.4.100
 if __name__ == '__main__':
-    #save_result()
+    request_url('166.111.4.100', '2402:f000:1:404:166:111:4:100')
+    # save_result()
     main()
     # get_addr('top-1m.csv');
 
     #    main()
-
